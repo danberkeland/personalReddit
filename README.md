88ec42d425942da9c45da3da377cac23a4e8c409
Dan Berkeland <danberkeland@gmail.com>
2021-01-02 08:55:56 -0800

    finished most of initial styling

--- README.md
+++ README.md
@@ -95,7 +95,7 @@ Upon attempting to start the script, I ran into a problem with the Jest dependen
 
 ```
 1. Delete package-lock.json (not package.json!) and/or yarn.lock in your project folder.
-  2. Delete node_modules in your project folder.
-  3. Remove "jest" from dependencies and/or devDependencies in the package.json file in your project folder.
-  4. Run npm install or yarn, depending on the package manager you use.
+2. Delete node_modules in your project folder.
+3. Remove "jest" from dependencies and/or devDependencies in the package.json file in your project folder.
+4. Run npm install or yarn, depending on the package manager you use.
 ```
--- personalreddit/src/App.js
+++ personalreddit/src/App.js
@@ -4,9 +4,94 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>Here's where the header goes</header>
-      <main>Here's where the body goes</main>
-      <nav>Here's where the navigation goes</nav>
+      <header>
+        <div className="logo"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
+        <div className="search">
+          <input type="text" placeholder="search..." />
+        </div>
+      </header>
+      <div className="mainmain">
+      <main>
+        <article>
+          <div className="karmaAdjust">Here's where karma adjust goes</div>
+          <div className="postInfo">
+            <h2>Post Title</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <div className = "postFooter">
+              <h3>Contributor</h3>
+              <div className = "timePosted">1 hr ago</div>
+              <div className = "commentCount">123</div>             
+            </div>
+            <div className = "commentContainer">
+              <h3>Commenting User</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+              <div className = "timePosted">1 hr ago</div>
+            </div>
+            <div className = "commentContainer">
+              <h3>Commenting User</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+              <div className = "timePosted">1 hr ago</div>
+            </div>
+            <div className = "commentContainer">
+              <h3>Commenting User</h3>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+              <div className = "timePosted">1 hr ago</div>
+            </div> 
+          </div>         
+        </article>
+        <article>
+          <div className="karmaAdjust">Here's where karma adjust goes</div>
+          <div className="postInfo">
+            <h2>Post Title</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+            
+            <div className = "postFooter">
+              <h3>Contributor</h3>
+              <div className = "timePosted">1 hr ago</div>
+              <div className = "commentCount">123</div>             
+            </div>
+          </div>         
+        </article>
+        <article>
+          <div className="karmaAdjust">Here's where karma adjust goes</div>
+          <div className="postInfo">
+            <h2>Post Title</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+           
+            <div className = "postFooter">
+              <h3>Contributor</h3>
+              <div className = "timePosted">1 hr ago</div>
+              <div className = "commentCount">123</div>             
+            </div>
+          </div>         
+        </article>
+        <button>BACK TO LIST</button>
+      </main>
+      <nav>
+        <h1>Subreddits</h1>
+        <div className="navItem">
+          <div className = "navIcon"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
+          <h2>Nav Title</h2>
+        </div>
+        <div className="navItem">
+          <div className = "navIcon"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
+          <h2>Nav Title</h2>
+        </div>
+        <div className="navItem">
+          <div className = "navIcon"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
+          <h2>Nav Title</h2>
+        </div>
+        <div className="navItem">
+          <div className = "navIcon"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
+          <h2>Nav Title</h2>
+        </div>
+        <div className="navItem">
+          <div className = "navIcon"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
+          <h2>Nav Title</h2>
+        </div>
+      </nav>
+      </div>
     </div>
   );
 }
