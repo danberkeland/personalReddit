fd7c6f29cef166576a815a7347ab8b346c04cf7f
Dan Berkeland <danberkeland@gmail.com>
2021-01-02 15:12:26 -0800

    built a starter nav menu

--- personalreddit/src/sections/nav.js
+++ personalreddit/src/sections/nav.js
@@ -9,8 +9,20 @@ export const Nav = () => {
         <nav>
             <h1>Subreddits</h1>
             <div className="navItem">
-            <div className = "navIcon"><img alt="Reddit icon" src="Reddit-Icon.png" /></div>
-            <h2>Nav Title</h2>
+            <div className = "navIcon"><img alt="r/history" src="history.png" /></div>
+            <h2>r/history</h2>
+            </div>
+            <div className="navItem">
+            <div className = "navIcon"><img alt="r/Breadit" src="breadit.jpg" /></div>
+            <h2>r/Breadit</h2>
+            </div>
+            <div className="navItem">
+            <div className = "navIcon"><img alt="r/ReactJS" src="reactjs.png" /></div>
+            <h2>r/ReactJS</h2>
+            </div>
+            <div className="navItem">
+            <div className = "navIcon"><img alt="r/HistoryAnecdotes" src="historyanecdotes.jpg" /></div>
+            <h2>r/History Anecdotes</h2>
             </div>
             
         </nav>
