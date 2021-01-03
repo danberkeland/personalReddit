8985cb5c30f8b9d030d34104b4aec12873acf573
Dan Berkeland <danberkeland@gmail.com>
2021-01-03 15:46:36 -0800

    OK, figured out photos roughly - need to wiki

--- personalreddit/src/sections/main.js
+++ personalreddit/src/sections/main.js
@@ -34,7 +34,16 @@ export const Main = (props) => {
     return finfin[0]
   }
 
+  const translate2 = (text) => {
 
+    let fin = text.replace('&lt;!-- SC_OFF --&gt;','').replace('&lt;!-- SC_ON --&gt;','').replace('class="md"','');
+    let finfin = ReactHtmlParser(fin)
+    if (finfin[0].endsWith('.jpg')){
+      return "<img src='"+finfin[0]+"'/>"
+  
+  }
+
+  }
 
   return (
       
@@ -49,7 +58,7 @@ export const Main = (props) => {
             <div className="postInfo">
               <h2>{item.data.title}</h2>
               {item.data.selftext_html ? <React.Fragment>{ReactHtmlParser (translate(item.data.selftext_html))}</React.Fragment> : <p>{item.data.selftext}</p>}
-              
+              {item.data.url ? <React.Fragment>{ReactHtmlParser (translate2(item.data.url))}</React.Fragment> : <p>{item.data.selftext}</p>}
               <Footer author={item.data.author} commentCount={item.data.num_comments} timePosted={item.data.created_utc}/>
               {/* <Comment /> */}
             </div>         
