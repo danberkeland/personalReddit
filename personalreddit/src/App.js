09ff958c364f69f901ef543a76f4cb1a644d67e1
Dan Berkeland <danberkeland@gmail.com>
2021-01-02 16:54:25 -0800

    Added some Router switches and links ... work in progress

--- personalreddit/src/App.js
+++ personalreddit/src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 import { Header } from './sections/header'
 import { Main } from './sections/main'
 import { Nav } from './sections/nav'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
  
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Header />
       <div className="mainmain">
-      <Main />
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/history" exact component={Main} />
+          <Route path="/Breadit" exact component={Main} />
+          <Route path="/ReactJS" exact component={Main} />
+          <Route path="/HistoryAnecdotes" exact component={Main} />
+        </Switch>
       <Nav />
       
+      </div>
     </div>
-    </div>
+    </Router>
+    
   );
 }
 
--- personalreddit/src/sections/nav.js
+++ personalreddit/src/sections/nav.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom';
+
 
 
 
@@ -8,22 +10,30 @@ export const Nav = () => {
     return (
         <nav>
             <h1>Subreddits</h1>
-            <div className="navItem">
-            <div className = "navIcon"><img alt="r/history" src="history.png" /></div>
-            <h2>r/history</h2>
-            </div>
-            <div className="navItem">
-            <div className = "navIcon"><img alt="r/Breadit" src="breadit.jpg" /></div>
-            <h2>r/Breadit</h2>
-            </div>
-            <div className="navItem">
-            <div className = "navIcon"><img alt="r/ReactJS" src="reactjs.png" /></div>
-            <h2>r/ReactJS</h2>
-            </div>
-            <div className="navItem">
-            <div className = "navIcon"><img alt="r/HistoryAnecdotes" src="historyanecdotes.jpg" /></div>
-            <h2>r/History Anecdotes</h2>
-            </div>
+            <Link to='/history'>
+                <div className="navItem">
+                <div className = "navIcon"><img alt="r/history" src="history.png" /></div>
+                <h2>r/history</h2>
+                </div>
+            </Link>
+            <Link to='/Breadit'>
+                <div className="navItem">
+                <div className = "navIcon"><img alt="r/Breadit" src="breadit.jpg" /></div>
+                <h2>r/Breadit</h2>
+                </div>
+            </Link>
+            <Link to='/ReactJS'>
+                <div className="navItem">
+                <div className = "navIcon"><img alt="r/ReactJS" src="reactjs.png" /></div>
+                <h2>r/ReactJS</h2>
+                </div>
+            </Link>
+            <Link to="/HistoryAnecdotes">
+                <div className="navItem">
+                <div className = "navIcon"><img alt="r/HistoryAnecdotes" src="historyanecdotes.jpg" /></div>
+                <h2>r/History Anecdotes</h2>
+                </div>
+            </Link>
             
         </nav>
     )
